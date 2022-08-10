3076adf6f44a1b377d048feb641e34286922962e
Keelan <keelanmatthews123@gmail.com>
2022-08-10 11:51:22 +0200

    Finished Task 1?

--- Practical 2/Transport.java
+++ Practical 2/Transport.java
@@ -1,13 +1,28 @@
 public class Transport extends Thread {
     Venue destination;
+	private Filter lock;
+	private int load = 0;
 
-	public Transport(Venue dest){
+	public Transport(Venue dest, Filter l){
 		destination = dest;
+		lock = l;
 	}
 
 	@Override
 	public void run()
 	{
-
+		String s = Thread.currentThread().getName();
+		int id = Integer.parseInt(s.substring(s.lastIndexOf("-") + 1));
+		for (int i = 0; i < 5; i++) {
+			load++;
+			System.out.println("BUS " + id + " is waiting to drop-off: LOAD " + load);
+			lock.lock();
+			try {
+				destination.dropOff(load);
+			} finally {
+				System.out.println("BUS " + id + " has left: LOAD " + load);
+				lock.unlock();
+			}
+		}
 	}
 }
--- Practical 2/Venue.java
+++ Practical 2/Venue.java
@@ -3,7 +3,15 @@ import java.util.concurrent.locks.Lock;
 public class Venue {
     Lock l;
 
-	public void dropOff(){
-
+	public void dropOff(int load){
+		String s = Thread.currentThread().getName();
+		int id = Integer.parseInt(s.substring(s.lastIndexOf("-") + 1));
+		System.out.println("BUS " + id + " is dropping off: LOAD " + load);
+		try {
+			Thread.sleep((long)(200 + Math.random() * 800));
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			Thread.currentThread().interrupt();
+		}
 	}
 }
