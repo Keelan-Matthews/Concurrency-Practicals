e190e5697a830fe43791f94d356a554f30a87ec5
Keelan <keelanmatthews123@gmail.com>
2022-08-11 10:31:19 +0200

    implemented other peterson overrides

--- Practical 1/Peterson.java
+++ Practical 1/Peterson.java
@@ -1,3 +1,4 @@
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 
 class Peterson implements Lock {
@@ -19,5 +20,18 @@ class Peterson implements Lock {
         String s = Thread.currentThread().getName();
         int id = Integer.parseInt(s.substring(s.lastIndexOf("-") + 1));
         flag[id] = false;
+    }
+
+    public void lockInterruptibly() throws InterruptedException {
+        throw new UnsupportedOperationException();
+    }
+    public boolean tryLock() {
+        throw new UnsupportedOperationException();
+    }
+    public boolean tryLock(long time, java.util.concurrent.TimeUnit unit) throws InterruptedException {
+        throw new UnsupportedOperationException();
+    }
+    public Condition newCondition() {
+        throw new UnsupportedOperationException();
     }
 }
