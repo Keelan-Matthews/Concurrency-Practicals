230419c7fcb171f95316d3fd4b73ec3d92f0f28e
Keelan <keelanmatthews123@gmail.com>
2022-10-06 11:46:27 +0200

    Finished task 3

--- Practical 5/Task 3/Gallery.java
+++ Practical 5/Task 3/Gallery.java
@@ -11,69 +11,101 @@ public class Gallery {
 
     public boolean add(String item, long timePeriod) {
         int key = item.hashCode();
-        head.lock();
-        Node pred = head;
-        try {
+        while (true) {
+            Node pred = head;
             Node curr = pred.next;
+            while (curr.key < key) {
+                pred = curr;
+                curr = curr.next;
+            }
+            pred.lock();
             curr.lock();
             try {
-                while (curr.key < key) {
-                    pred.unlock();
-                    pred = curr;
-                    curr = curr.next;
-                    curr.lock();
+                if (validate(pred, curr)) {
+                    if (curr.key == key) {
+                        return false;
+                    } else {
+                        Node node = new Node(item, timePeriod);
+                        node.next = curr;
+                        pred.next = node;
+                        return true;
+                    }
                 }
-                if (key == curr.key) {
-                    return false;
-                } 
-                Node node = new Node(item, timePeriod);
-                node.next = curr;
-                pred.next = node;
-                return true;
             } finally {
+                pred.unlock();
                 curr.unlock();
             }
-        } finally {
-            pred.unlock();
         }
     }
 
     public boolean remove(String item) {
-        Node pred = null, curr = null;
         int key = item.hashCode();
-        head.lock();
-        try {
-            pred = head;
-            curr = pred.next;
+        while (true) {
+            Node pred = head;
+            Node curr = pred.next;
+            while (curr.key < key) {
+                pred = curr;
+                curr = curr.next;
+            }
+            pred.lock();
             curr.lock();
             try {
-                while (curr.key < key) {
-                    pred.unlock();
-                    pred = curr;
-                    curr = curr.next;
-                    curr.lock();
+                if (validate(pred, curr)) {
+                    if (curr.key == key) {
+                        pred.next = curr.next;
+                        return true;
+                    } else {
+                        return false;
+                    }
                 }
-                if (key == curr.key) {
-                    pred.next = curr.next;
-                    return true;
-                } 
-                return false;
             } finally {
+                Node temp = head.next;
+                System.out.print("Thread-" + Thread.currentThread().getName() + ": ");
+                while (temp.next != null) {
+                    String sID = temp.item.substring(0,1);
+                    String person = temp.item.substring(1);
+                    if (Integer.parseInt(sID) == Integer.parseInt(Thread.currentThread().getName())) {
+                        System.out.print("(P-" + person + ", " + (temp.timePeriod - System.currentTimeMillis()) + "ms) ");
+                    }
+                    temp = temp.next;
+                }
+                System.out.println("");
+                pred.unlock();
                 curr.unlock();
             }
-        } finally {
-            Node temp = head.next;
-            System.out.print("Thread-" + Thread.currentThread().getName() + ": ");
-            while (temp.next != null) {
-                String sID = temp.item.substring(0,1);
-                String person = temp.item.substring(1);
-                if (Integer.parseInt(sID) == Integer.parseInt(Thread.currentThread().getName())) {
-                    System.out.print("(P-" + person + ", " + (temp.timePeriod - System.currentTimeMillis()) + "ms) ");
-                }
-                temp = temp.next;
+        }
+    }
+
+    private boolean validate(Node pred, Node curr) {
+        Node node = head;
+        while (node.key <= pred.key) {
+            if (node == pred) {
+                return pred.next == curr;
             }
-            System.out.println("");
-            pred.unlock();
+            node = node.next;
+        }
+        return false;
+    }
+
+    public boolean contains(String item) {
+        int key = item.hashCode();
+        while(true) {
+            Node pred = this.head;
+            Node curr = pred.next;
+            while (curr.key < key) {
+                pred = curr;
+                curr = curr.next;
+            }
+            pred.lock();
+            curr.lock();
+            try {
+                if (validate(pred, curr)) {
+                    return curr.key == key;
+                }
+            } finally {
+                pred.unlock();
+                curr.unlock();
+            }
         }
     }
 
